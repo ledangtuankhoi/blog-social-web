9971ac7d79906c25bb58caedf8d1806523512a69
huynhimo635 <huynh.nguyen.imo@gmail.com>
2022-02-19 22:34:03 +0700

    move folders to inside src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ export default function Home() {
         <title>Home Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="">Alooo</h1>
+
+      <h1 className="flex bg-slate-600 text-white text-2xl">Alooo</h1>
     </div>
   )
 }
